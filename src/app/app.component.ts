cd91c39ac57e2b603f4d3fc088195ea5e056eb39
Jeffrey Pieksma <piek1500@student.nhl.nl>
2018-09-24 15:57:02 +0200

    Server components, bootstrap, app components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h1{color:green;
+    }
+  `]
 })
-export class AppComponent {
+
+export class AppComponent {   
+
   title = 'angularApp';
+  name = 'My name is jeff';
+  showPara = true;  
+  eventsArray = [];   
+  counter = 1;    
+  
+  togglePara(event: Event) {       
+    this.showPara = !this.showPara;       
+    this.eventsArray.push(event.type);    
+  }
 }
